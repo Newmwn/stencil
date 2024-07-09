34034ea721c8f7bd4383680085b8354a6de098a1
Andre Dias <andre.dias@niup.com>
2024-07-09 09:08:36 +0100

    up

--- src/components/np-slider/np-slider.tsx
+++ src/components/np-slider/np-slider.tsx
@@ -1,5 +1,4 @@
 import { Component, Element, Event, EventEmitter, h, Prop, State, Watch } from '@stencil/core';
-import { ShadowDomHelper } from '../../utils/shadow-dom-helper';
 
 @Component({
   tag: 'np-slider',
@@ -8,29 +7,30 @@ import { ShadowDomHelper } from '../../utils/shadow-dom-helper';
 })
 export class NpSlider {
   @Element() npElement;
+  fromHandler: HTMLInputElement;
+  toHandler: HTMLInputElement;
 
   _value: number | number[] = 0;
-  @Prop({ attribute: 'value' }) value: number | number[] = [10];
+  @Prop({ attribute: 'value' }) value: number | number[] = [10, 30];
   @Watch('value') watchValue(newValue: number | number[]) {
     if (!this.range) {
       this._value = typeof newValue === 'number' ? newValue : newValue[0];
     } else {
       this._value = newValue.constructor === Array && newValue.length < 3 && newValue.length > 0 ? (newValue.length > 1 ? newValue : [0, newValue[0]]) : [0, 0];
     }
-    console.log('%csrccomponents\np-slider\np-slider.tsx:20 this._value', 'color: #007acc;', this._value);
   }
 
   @Prop({ attribute: 'disabled' }) disabled: boolean = false;
 
-  @Prop() min: number = 0;
+  @Prop({ attribute: 'min' }) min: number = 0;
 
-  @Prop() max: number = 100;
+  @Prop({ attribute: 'max' }) max: number = 100;
 
-  @Prop() orientation: string = 'horizontal';
+  @Prop({ attribute: 'orientation' }) orientation: string = 'horizontal';
 
-  @Prop() step: number;
+  @Prop({ attribute: 'step' }) step: number;
 
-  @Prop() range: boolean = true;
+  @Prop({ attribute: 'range' }) range: boolean = true;
 
   @Prop() sliderStyle: any;
 
@@ -40,22 +40,17 @@ export class NpSlider {
 
   @Prop() tabindex: number = 0;
 
-  @Event() onChange: EventEmitter<any>;
+  @Event() changeEvent: EventEmitter<any>;
 
-  @Event() onSlideEnd: EventEmitter<any>;
+  @Event() slideEndEvent: EventEmitter<any>;
 
   componentWillLoad() {
     this.watchValue(this.value);
   }
 
   componentDidLoad() {
-    const fromSlider = ShadowDomHelper.searchElement(this.npElement, '#fromSlider') as HTMLInputElement;
-    const toSlider = ShadowDomHelper.searchElement(this.npElement, '#toSlider') as HTMLInputElement;
-    this.fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
-    this.setToggleAccessible(toSlider);
-
-    fromSlider.oninput = () => this.controlFromSlider(fromSlider, toSlider);
-    toSlider.oninput = () => this.controlToSlider(fromSlider, toSlider);
+    this.fillSlider(this.fromHandler, this.toHandler, '#C6C6C6', '#25daa5', this.toHandler);
+    this.setToggleAccessible(this.toHandler);
   }
 
   controlFromSlider(fromSlider: HTMLInputElement, toSlider: HTMLInputElement) {
@@ -65,6 +60,7 @@ export class NpSlider {
     if (from > to) {
       fromSlider.value = to.toString();
     }
+    this._value = !this.range ? from : [from, to];
   }
 
   controlToSlider(fromSlider: HTMLInputElement, toSlider: HTMLInputElement) {
@@ -77,6 +73,8 @@ export class NpSlider {
     } else {
       toSlider.value = from.toString();
     }
+    this._value = !this.range ? to : [from, to];
+    this.changeEvent.emit(this._value);
   }
 
   getParsed(currentFrom: HTMLInputElement, currentTo: HTMLInputElement): [number, number] {
@@ -102,11 +100,10 @@ export class NpSlider {
 
   setToggleAccessible(currentTarget: HTMLInputElement): void {
     if (this.disabled) return;
-    const toSlider = ShadowDomHelper.searchElement(this.npElement, '#toSlider') as HTMLInputElement;
     if (Number(currentTarget.value) <= 0) {
-      toSlider.style.zIndex = '2';
+      this.toHandler.style.zIndex = '2';
     } else {
-      toSlider.style.zIndex = '0';
+      this.toHandler.style.zIndex = '0';
     }
   }
 
@@ -116,10 +113,28 @@ export class NpSlider {
   //#region RENDER
   render() {
     return (
-      <div class="range_container">
-        <div class="sliders_control">
-          <input disabled={this.disabled} style={{ visibility: 'hidden' }} id="fromSlider" type="range" defaultValue="0" min="0" max="100" />
-          <input disabled={this.disabled} id="toSlider" type="range" defaultValue="30" min="0" max="100" />
+      <div class="slider-container">
+        <div class="slider-container--slider-control">
+          <input
+            ref={fromHandlerRef => (this.fromHandler = fromHandlerRef)}
+            class="slider-container--slider-control__fromHandler"
+            style={{ display: !this.range ? 'none' : 'unset' }}
+            disabled={this.disabled}
+            type="range"
+            defaultValue={!this.range ? 0 : this._value[0]}
+            min={this.min}
+            max={this.max}
+            onInput={() => this.controlFromSlider(this.fromHandler, this.toHandler)}
+          />
+          <input
+            ref={toHandlerRef => (this.toHandler = toHandlerRef)}
+            disabled={this.disabled}
+            type="range"
+            defaultValue={!this.range ? this._value : this._value[1]}
+            min={this.min}
+            max={this.max}
+            onInput={() => this.controlToSlider(this.fromHandler, this.toHandler)}
+          />
         </div>
       </div>
     );
